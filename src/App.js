031b2ce861b08f2833870fe134363822f16e4576
Vladislav Kulachko <vladcoool@gmail.com>
2021-08-14 17:34:35 +0300

    завершает первую задачу

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import './friend-list/friends-list';
+import Profile from './social-profile/social-profile';
+import './statistics/statistics';
+import './transaction-history/transaction-history';
+
+import user from '../src/social-profile/user.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   );
 }
 
